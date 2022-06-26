a5cfcad4841e754f3e268203790e39d208dcb30b
ajeuun <ajeeun@github.com>
2022-06-26 23:09:09 +0900

    first commit

---
+++ src/main/java/sanwon/jpashop/Main.java
@@ -0,0 +1,11 @@
+package sanwon.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Main {
+
+    private  String data;
+}
---
+++ src/main/java/sanwon/jpashop/MainController.java
@@ -0,0 +1,17 @@
+package sanwon.jpashop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("main")
+    public String hello(Model model){
+        model.addAttribute("data","메인화면");
+        return "main";
+
+    }
+
+}
